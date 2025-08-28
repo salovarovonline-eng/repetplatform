3346f7262ae1cecf9c98d99306c287000b9a8a94
salovarovonline-eng <salovarovonline@gmail.com>
2025-08-28 15:08:17 +0800

    chore: remove versioned import paths

--- src/components/TutorDashboard.tsx
+++ src/components/TutorDashboard.tsx
@@ -7,7 +7,7 @@ import { Input } from './ui/input'
 import { Label } from './ui/label'
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from './ui/select'
 import { Textarea } from './ui/textarea'
-import { toast } from 'sonner@2.0.3'
+import { toast } from 'sonner'
 import { 
   UserPlus, 
   Calendar, 
